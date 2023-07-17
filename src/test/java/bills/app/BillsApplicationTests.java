cae4374bfba192a0e0d629a6b1f282d9cce84d7f
vzavolsky <vzavolsky@ya.ru>
2023-07-17 20:20:48 +0300

    Initial commit.

---
+++ src/test/java/bills/app/BillsApplicationTests.java
@@ -0,0 +1,13 @@
+package bills.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
